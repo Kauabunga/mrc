daa8de096f7a1aece6d72b91e1b8d346db660557
Carson Bruce <carson.bruce@solnet.co.nz>
2017-08-18 22:54:30 +1200

    added

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-snapshot';
 import { Provider } from 'react-redux';
-import './index.css';
 
 import store from './global/store';
 import routes from './global/routes';
 
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(
+render(
   <Provider store={store}>
     {routes}
   </Provider>,
   document.getElementById('root'),
 );
+
 registerServiceWorker();
