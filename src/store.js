020164d072e2f609eeb3cffab199fc6f7ff20502
Carson Bruce <carson_bruce@bnz.co.nz>
2017-08-18 13:19:14 +1200

    Added dev tools

--- src/store.js
+++ src/store.js
@@ -1,21 +1,39 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
 import createSagaMiddleware from 'redux-saga';
-
 import createGlobalReducer from './global-reducer';
 import globalSagas from './global-sagas';
 
 export const history = createHistory();
 const sagaMiddleware = createSagaMiddleware();
 
+const rootReducer = createGlobalReducer();
+const enhancers = [];
 const middlewares = [
   // Middleware for intercepting and dispatching navigation actions
   routerMiddleware(history),
   sagaMiddleware,
 ];
 
-const store = createStore(createGlobalReducer(), applyMiddleware(...middlewares));
+// Redux dev tools
+if (process.env.NODE_ENV === 'development') {
+  const devToolsExtension = window.devToolsExtension;
+  if (typeof devToolsExtension === 'function') {
+    enhancers.push(devToolsExtension());
+  }
+}
+
+const composedEnhancers = compose(
+  applyMiddleware(...middlewares),
+  ...enhancers
+);
+
+const store = createStore(
+  rootReducer,
+  undefined,
+  composedEnhancers,
+);
 
 sagaMiddleware.run(globalSagas);
 
